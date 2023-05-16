5159ea6e5fb2219174ef0434146b2ac276ec4e83
ispervoykin <83460726+ispervoykin@users.noreply.github.com>
2023-05-17 00:56:14 +0300

    Update sender.py

--- homework03/sender.py
+++ homework03/sender.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 
 name = input("Введите имя: ")
@@ -13,22 +12,3 @@ while True:
             "text": text
         }
     )
-
-=======
-import requests
-
-name = input("Введите имя: ")
-
-while True:
-    text = input("Введите текст: ")
-
-    response = requests.post(
-        'http://127.0.0.1:5000/send',
-        json = {
-            "name": name,
-            "text": text
-        }
-    )
-
->>>>>>> 9da9ad5897043b2dc2b400e92df224257804c46f
-print(response.text)
